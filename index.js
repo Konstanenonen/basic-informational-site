cb4b2663fb5e84bd84135ab53f91e05b6e3f3edb
Konsta Nenonen <konstanenonen@protonmail.com>
2022-11-28 22:45:34 +0200

    Added one more basic route and a error route that shows if any of the others dont match

--- index.js
+++ index.js
@@ -34,5 +34,31 @@ https
         return res.end();
       });
     }
+
+    if (pathName === '/contact-me') {
+      fs.readFile('./contact-me.html', (err, data) => {
+        if (err) {
+          res.writeHead(404, { 'Conten-Type': 'text/html' });
+          return res.end('404 There is nothing here');
+        }
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+      });
+    }
+
+    if (pathName !== '/' && pathName !== '/about') {
+      fs.readFile('./404.html', (err, data) => {
+        if (err) {
+          res.writeHead(404, { 'Conten-Type': 'text/html' });
+          return res.end('404 There is nothing here');
+        }
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+      });
+    }
   })
   .listen(8080);
